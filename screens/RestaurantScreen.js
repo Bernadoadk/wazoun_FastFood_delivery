0872499bf50b1147907198ac37302cc8c59dfceb
Bernadoadk <bernado.adikpeto@epitech.eu>
2024-10-13 10:24:05 +0100

    RestaurantScreen add

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -21,6 +21,14 @@ export default function RestaurantScreen() {
             <Icon.ArrowLeft strokeWidth={3} stroke={themeColors.bgColor(1)} />
           </TouchableOpacity>
         </View>
+        <View
+          style={{borderTopLeftRadius: 40, borderTopRightRadius: 40}}
+          className="bg-white -mt-12 pt-6"
+        >
+          <View className="px-5">
+            <Text className="text-3xl font-bold">{item.name}</Text>
+          </View>
+        </View>
       </ScrollView>
     </View>
   )
